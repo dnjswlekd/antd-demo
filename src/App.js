dba8bac5c9ac5b2281a9a1901c0d84c6f50d60b6
dnjswlekd <wj8014@gmail.com>
2025-05-27 14:40:38 +0900

    button loading state

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { PoweroffOutlined, SyncOutlined } from '@ant-design/icons';
 import { Button, Space, DatePicker, version } from 'antd';
 
 const App = () => {
+  const [loading, setLoading] = useState(false);
+
   const onButtonClick = (e) => {
     console.log('click');
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 2000);
   };
 
   return (
@@ -11,7 +18,15 @@ const App = () => {
       <h1>antd version: {version}</h1>
       <Space>
         {/* <DatePicker /> */}
-        <Button type="primary" block onClick={onButtonClick}>
+        <Button
+          type="primary"
+          block
+          onClick={onButtonClick}
+          loading={loading}
+          className="primary-button"
+          icon={<PoweroffOutlined />}
+          style={{ backgroundColor: 'skyBlue' }}
+        >
           Primary Button
         </Button>
       </Space>
